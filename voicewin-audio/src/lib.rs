e0bec87877ea30cb13876a7bde369be0f76c8a00
Clouder0 <clouder0@outlook.com>
2026-02-23 16:25:00 +0800

    fix(core): macOS fix

--- voicewin-audio/src/lib.rs
+++ voicewin-audio/src/lib.rs
@@ -5,3 +5,6 @@ mod recorder;
 
 #[cfg(any(windows, target_os = "macos"))]
 pub use recorder::{AudioCaptureError, AudioRecorder, CapturedAudio};
+
+#[cfg(any(windows, target_os = "macos"))]
+pub use recorder::AudioInputDeviceInfo;
